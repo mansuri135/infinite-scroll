bd881f733203076f95bc5ee2d11011dc293d519c
K Harsh <harshnaidu2u@gmail.com>
2023-12-22 19:55:15 +0530

    INFINITE scroll, useEffect, useState

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Build Modern Next 14 Server Side App with Server Actions, Infinite Scroll & Framer Motion Animations
 
 ![Anime Website](https://i.ibb.co/MG1nbqt/YT-Thumbnails-2.png)
+
+## NPM packages
+
+1. react-intersection-observer
--- components/LoadMore.tsx
+++ components/LoadMore.tsx
@@ -1,10 +1,40 @@
+"use client";
+// only this will load in server side
+
+import { useEffect, useState } from "react";
 import Image from "next/image";
+import { useInView } from "react-intersection-observer";
+import { fetchAnime } from "@/app/action";
+import AnimeCard, { AnimeProp } from "./AnimeCard";
+
+let page = 2; // to fetch new pages
 
 function LoadMore() {
+  const { ref, inView } = useInView();
+  const [data, setData] = useState<AnimeProp[]>([]); // to get an array of AnimeProps
+
+  useEffect(() => {
+    if (inView) {
+      // alert("load lawde");
+      fetchAnime(page).then((res) => {
+        setData([...data, ...res]);
+
+        page++; // increase page numbers
+      });
+    }
+  }, [inView, data]); // when the spinner hits it run execute
+
   return (
     <>
+      <section className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-10">
+        {data.map((item: AnimeProp, index: number) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
+      {/* this section is for all of the subsequent pages from page one */}
+
       <section className="flex justify-center items-center w-full">
-        <div>
+        <div ref={ref}>
           <Image
             src="./spinner.svg"
             alt="spinner"
