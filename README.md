382ab5e9ce4e7f1699d74e265824f0130b270e0b
minhaj135 <minhaj.mansuri9298@gmail.com>
2024-04-01 09:11:57 +0530

    first commit

--- README.md
+++ README.md
@@ -1,8 +1,1 @@
-# Build Modern Next 14 Server Side App with Server Actions, Infinite Scroll & Framer Motion Animations
-
-![Anime Website](https://i.ibb.co/MG1nbqt/YT-Thumbnails-2.png)
-
-## NPM packages
-
-1. react-intersection-observer
-2. framer-motion
+Infinite Scroll
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,14 +3,8 @@ import Image from "next/image";
 function Footer() {
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
-      <Image
-        src="./logo.svg"
-        alt="logo"
-        width={47}
-        height={44}
-        className="object-contain"
-      />
+      <p className="text-base font-bold text-white">@2024 All rights reserved</p>
+      
       <div className="flex items-center gap-6">
         <Image
           src="./tiktok.svg"
